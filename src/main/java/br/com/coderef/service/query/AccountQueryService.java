a55ff2a1285d7a0b3bad513f5b12e424f1e75c1c
israelnp <israelpereira1984@gmail.com>
2021-11-22 18:54:19 -0300

    add getAccount method

--- src/main/java/br/com/coderef/service/query/AccountQueryService.java
+++ src/main/java/br/com/coderef/service/query/AccountQueryService.java
@@ -1,7 +1,10 @@
 package br.com.coderef.service.query;
 
+import br.com.coderef.entity.AccountQueryEntity;
+
 import java.util.List;
 
 public interface AccountQueryService {
     public List<Object> listEventsForAccount(String accountNumber);
+    public AccountQueryEntity getAccount(String accountNumber);
 }
